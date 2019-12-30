cc63eac0b07c69c164e86e845531582ccee85f3b
Lee Bardon <leerbardon@gmail.com>
2019-12-30 23:33:40 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,17 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_pigeons = {}
+  data.each do |attributes, values|
+    values.each do |keys_1, names_arrays|
+      names_arrays.each do |name|
+        if !new_pigeons[name]
+          new_pigeons[name] = {}
+        end
+        if !new_pigeons[name][attributes]
+          new_pigeons[name][attributes] = []
+        end
+        new_pigeons[name][attributes].push(keys_1.to_s)
+      end
+    end
+  end
+  pp new_pigeons
 end
